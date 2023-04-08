b460b2e5eb2507bf165c7672faea73a8207bc6b8
stas <point.blank.point@bk.ru>
2023-04-08 18:58:49 +0300

    exz1 done

---
+++ exz1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] CreateArray(int length, int minRnd, int maxRnd)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+int pozitive(int[] array)
+{
+    int hetn = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (hetn == array[i] / 2)
+            hetn++;
+    }
+    return hetn;
+}
+int len = ReadInt("Введите длину массива");
+int minRnd = ReadInt("Введите границу минимума случайной ведичины");
+int maxRnd = ReadInt("Введите границу максимума случайной ведичины");
+int[] array = CreateArray(len, minRnd, maxRnd);
+PrintArray(array);
+int per;
+per = pozitive(array);
+System.Console.WriteLine($"Количество положительных числе в массиве  ");
+System.Console.WriteLine($"{per}");
