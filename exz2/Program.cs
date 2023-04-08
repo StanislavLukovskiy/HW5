acab2db4502551338207e75edadbf111f7eeb982
stas <point.blank.point@bk.ru>
2023-04-08 19:43:37 +0300

    exz2 done

--- exz2/Program.cs
+++ exz2/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int ReadInt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] CreateArray(int length, int minRnd, int maxRnd)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(minRnd, maxRnd + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int negativSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        sum = sum + array [i];
+    }
+    return sum;
+}
+int len = ReadInt("Введите длину массива");
+int minRnd = ReadInt("Введите границу минимума случайной ведичины");
+int maxRnd = ReadInt("Введите границу максимума случайной ведичины");
+int[] array = CreateArray(len, minRnd, maxRnd);
+PrintArray(array);
+int per;
+per = negativSum(array);
+System.Console.WriteLine($"Сумма чисел на нечётных позициях в массиве  ");
+System.Console.WriteLine($"{per}");
